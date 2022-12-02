5eff6f54435f964ad891bef486dfa3d43463bedd
laukhin-alex <alex.laukhin@yandex.ru>
2022-12-02 22:06:43 +0400

    Initial Commit

---
+++ CatsAndDogs/CatsAndDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndDogsApp.swift
+//  CatsAndDogs
+//
+//  Created by Александр on 02.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
